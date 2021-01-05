a5d07f2b1ab0090738acd62028ce71767fb14e8f
harold maussa <ricardo.baquero@ext.grupokonecta.com>
2021-01-05 18:41:04 -0500

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::group([
-    'prefix' => 'auth'
+    'prefix' => 'auth', 'middleware' => ['cors']
 ], function () {
     Route::post('login', 'AuthController@login');
     Route::post('signup', 'AuthController@signUp');
